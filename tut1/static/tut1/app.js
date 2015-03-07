87525b5577b959b2554fb7f89cf37322850dd5e1
Dmitriy <dima@localhost.localdomain>
2015-03-07 17:42:51 -0500

    using ng-repeat

--- tut1/static/tut1/app.js
+++ tut1/static/tut1/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('store', []);
+(function (){
+	var app = angular.module('store', []);
+
+	app.controller('StoreController', function(){
+		this.products=gems;
+	});
+
+	var gems = [
+		{name: 'Dodecahedron',
+		price: 2.95,
+		description: 'The dodecahedron gem'},
+		{name: 'Other gem 1',
+		price: 3.5,
+		description: 'Other gem'},
+		{name: 'Other gem 2',
+		price: 4,
+		description: 'Another gem'}
+		]
+		
+})();
