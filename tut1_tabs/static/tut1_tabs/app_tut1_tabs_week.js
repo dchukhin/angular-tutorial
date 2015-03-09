be2857bd033c599bb014dfe3780958a0cae109f8
Dmitriy <dima@localhost.localdomain>
2015-03-09 15:38:38 -0400

    added methods for setting and checking tab to js file

--- tut1_tabs/static/tut1_tabs/app_tut1_tabs_week.js
+++ tut1_tabs/static/tut1_tabs/app_tut1_tabs_week.js
@@ -5,6 +5,21 @@
 		this.days = days;
 	});
 
+	app.controller('TabController', function(){
+		/*Tab defined as 1(Monday) at beginning.*/
+		this.tab=1;
+
+		/*Method to set the current tab. Used when user clicks on a tab.*/
+		this.setTab = function(tabToSet){
+			this.tab = tabToSet;
+		};
+
+		/*Method to check if the current tab is set. Used to display/highlight
+ 		the current tab.*/
+		this.isTabSet = function(tabToCheck){
+			return (this.tab === tabToCheck);
+		};
+	});
 
 	var days = [
 	{day: 'Monday',
