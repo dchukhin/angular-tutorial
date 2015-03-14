f96f711117a9e09f7aad3bc8a30bb1b07c737160
Dmitriy <dima@localhost.localdomain>
2015-03-14 15:06:56 -0400

    load from js file with directives

---
+++ tut1_tabs/static/tut1_tabs/app_tut1_tabs_review_form_directives.js
@@ -0,0 +1,58 @@
+(function (){
+	var app = angular.module('store', []);
+
+	app.controller('StoreController', function(){
+		this.products=gems;
+	});
+
+	app.controller('TabController', function(){
+		this.tab = 1;
+
+		this.setTab = function(tabToSet){
+			this.tab = tabToSet;
+		};
+		
+		this.isTabSet = function(tabToCheck){
+			return this.tab === tabToCheck;
+		};
+	});
+	
+	/*Our review controller takes the current product as the only parameter.
+    *Then it adds the current review to the end of the product reviews.*/
+	app.controller('ReviewController', function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			/*Now clear the review form so that it is blank for next review.*/
+			this.review = {};
+		};
+	});
+	
+
+	var gems = [
+		{name: 'Dodecahedron',
+		price: 2.95,
+		description: 'The dodecahedron gem',
+		reviews: [
+			{stars:5, 
+			body: 'Great gem!', 
+			author: 'person@email.com'},
+			{stars:1,
+			body: 'Bad gem!',
+			author: 'other_person@email.com'}] 
+		},
+		{name: 'Other gem 1',
+		price: 3.5,
+		description: 'Other gem',
+		reviews: [{
+			stars: 3,
+			body: "Eh, it's ok",
+			author: 'someone@here.com'}]
+		},
+		{name: 'Other gem 2',
+		price: 4,
+		description: 'Another gem'}
+		]
+		
+})();
